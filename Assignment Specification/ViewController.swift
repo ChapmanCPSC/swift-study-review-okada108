17787930922c3a7cb89960f086c67776854dd3b9
Bryan Okada <bryanokada@bryans-macbook-pro-5.local>
2016-03-30 02:16:48 -0700

    Bryan Okada's Assignment Specification Assignment

--- Assignment Specification/ViewController.swift
+++ Assignment Specification/ViewController.swift
@@ -9,6 +9,94 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var lbl_topic: UILabel!
+    
+    @IBOutlet var lbl_timer: UILabel!
+    
+    
+    
+    var topics: [String] = ["Let vs Var", "Computed Initializers", "Computed Variables", "Setter Observeers", "Functions", "External Parameters", "Default Parameters", "Anonymous Functions", "Optional Chaining", "Failable Initializers", "Casting", "Cast Methods", "Extensions", "Enumerations", "Error Handling", "Dictionaries", "Conditional Binding", "Protocols", "Inheritence", "Structs", "Enums", "Classes" , "Subscripts", "Setter Observers"]
+    var time = 30
+    var time_run = false
+    var timer = NSTimer()
+    
+    
+    func time_count ()
+    {
+        if time == 0
+        {
+            lbl_topic.text = topics[rand_num()]
+            time = 30
+            lbl_timer.text = "\(time)"
+        }
+        else{
+            time -= 1
+            lbl_timer.text = "\(time)"
+        }
+    }
+    
+    func start_count()
+    {
+        if time_run == false{
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self , selector: Selector("time_count"), userInfo: nil, repeats: true)
+            time_run = true
+        }
+        
+    }
+    
+    
+    func rand_num () -> Int
+    {
+        var x = Int(arc4random_uniform(24))
+        return x
+    }
+    
+    
+    
+    @IBAction func btn_next(sender: AnyObject) {
+            lbl_topic.text = topics[rand_num()]
+        
+        
+        time = 30
+            
+        
+        
+
+        
+    }
+    
+    @IBAction func btn_start(sender: AnyObject) {
+        if time_run == false{
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self , selector: Selector("time_count"), userInfo: nil, repeats: true)
+            time_run = true
+            
+        }
+        
+        
+
+        
+        
+    }
+    
+    
+    @IBAction func btn_stop(sender: AnyObject) {
+        
+        if time_run == true{
+            timer.invalidate()
+            time_run = false
+            
+        }
+    }
+    
+    
+  
+    
+   
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +107,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
