ce3e2732cd43f0182e17705d9fd4b11aeefdbaad
Bryan Okada <bryanokada@bryans-macbook-pro-5.local>
2016-03-30 00:13:03 -0700

    Initial Commit

---
+++ Assignment Specification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment Specification
+//
+//  Created by Bryan Okada on 3/30/16.
+//  Copyright © 2016 Bryan Okada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
